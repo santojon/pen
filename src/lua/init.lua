33c9145c2fc720e3f80337081b2d3ad1708737e3
Jonathan santos <santojon5@gmail.com>
2016-03-18 17:02:49 -0300

    step back

--- src/lua/init.lua
+++ src/lua/init.lua
@@ -1,15 +1,35 @@
 local application = require("pen.application")
-local lapisApp = require("lapis.application")
 local App
 App = application.App
-local app_cache = lapisApp.app_cache
+local app_cache = { }
+setmetatable(app_cache, {
+  __mode = "k"
+})
+local dispatcher
 local serve
-do
-  local _base_0 = lapisApp
-  local _fn_0 = _base_0.serve
-  serve = function(...)
-    return _fn_0(_base_0, ...)
+serve = function(app_cls)
+  local app = app_cache[app_cls]
+  if not (app) then
+    local name = app_cls
+    if type(name) == "string" then
+      app_cls = require(name)
+    end
+    if app_cls.__base then
+      app = app_cls()
+    else
+      app_cls:build_router()
+      app = app_cls
+    end
+    app_cache[name] = app
   end
+  if not (dispatcher) then
+    if __leda then
+      dispatcher = require("lapis.leda")
+    else
+      dispatcher = require("lapis.nginx")
+    end
+  end
+  return dispatcher.dispatch(app)
 end
 return {
   serve = serve,
